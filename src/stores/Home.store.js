cc80286412f4e44c30049370c8c78944e67fc26d
BCollignonEcv <baptiste.collignon@mail-ecv.fr>
2022-06-21 14:11:30 +0200

    first commit

---
+++ src/stores/Home.store.js
@@ -0,0 +1,14 @@
+import { defineStore } from 'pinia'
+
+export const useHomeStore = defineStore({
+    id: 'home',
+    state: () => ('home', {
+        home: [],
+    }),
+    getters: {
+        
+    },
+    actions: {
+        
+    }
+});
