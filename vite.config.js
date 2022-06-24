965b6052051aa469e86e3c2d25d5640629e2b33d
BCollignonEcv <baptiste.collignon@mail-ecv.fr>
2022-06-24 15:06:36 +0200

    add deploy

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
                 import.meta.url))
         }
     },
-    base: '/',
+    base: '/museeArtHistoireHandicap/',
     configureWebpack: {
         module: {
             rules: [{
